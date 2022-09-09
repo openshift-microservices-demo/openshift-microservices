d7d9ae17a3e704a8233ab46dbca83b4a7e5cbcc4
Preston Davis <pdavis@redhat.com>
2022-09-09 12:20:19 -0400

    Cleaned up the blank spaces in the create-namespace.sh file

--- automation/create-namespace.sh
+++ automation/create-namespace.sh
@@ -2,19 +2,17 @@
 
 # Define Namespace variable
 NAMESPACE=$1
-
+#
 # Check if the namespace already exists
 if oc get namespace -o json | jq -r ".items[].metadata.name" | grep $NAMESPACE; then \
   echo "The Namespace $NAMESPACE already exists" 
 else
-# The above code gets superceded by the 'oc' command
-
+#
 # Create the Namespace
 echo "apiVersion: v1
 kind: Namespace
 metadata:
   name: ${NAMESPACE}" | oc apply -f -
-# Finish Namespace execution
 fi
 #
 # Modify privileges for the defaut service account in scc. This step needs to be reviewed as the gives the service account too much privileges.
